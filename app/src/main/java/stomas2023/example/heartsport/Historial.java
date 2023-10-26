2cd2e6f657773470d98a994a6a352ea8a3547fec
sistemas <vitocomix28@gmail.com>
2023-10-26 16:45:42 -0300

    Initial commit

---
+++ app/src/main/java/stomas2023/example/heartsport/Historial.java
@@ -0,0 +1,14 @@
+package stomas2023.example.heartsport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Historial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial);
+    }
+}
